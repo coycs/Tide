1e854d13e541fb1c4745c305407981e575d9783f
coycs <coycs@qq.com>
2023-02-04 21:39:26 +0800

    use process env

--- posts/subscribe/config.js
+++ posts/subscribe/config.js
@@ -1,12 +1,12 @@
 export default {
   // "domain": rsshub_domain,
-  "domain": rsshub_domain,
+  "domain": process.env.rsshub_domain,
   "rss2json": {
     "method": "get",
     "url": "https://api.rss2json.com/v1/api.json",
     "params": {
       "rss_url": "",
-      "api_key": rss2json_api_key,
+      "api_key": process.env.rss2json_api_key,
       "order_by": "pubDate",
       "count": 5
     }
